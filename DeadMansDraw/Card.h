e2385d28c703417f093bcc9ec89d3be483639b0f
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 02:22:51 +0930

    Add base card class virtual functions

--- DeadMansDraw/Card.h
+++ DeadMansDraw/Card.h
@@ -5,3 +5,16 @@
 */
 
 #include <iostream>
+
+class Game;
+class Player;
+
+class Card {
+public:
+    virtual void play(Game& game, Player& player) = 0;
+    virtual void willAddToBank(Game& game, Player& player) = 0;
+    virtual int getValue() const = 0;
+    virtual std::string str() const = 0;
+    virtual CardType Type() const = 0;
+
+};

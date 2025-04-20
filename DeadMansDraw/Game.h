b8320143bc902e90e13372f9142248dec322dfc2
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 05:05:41 +0930

    Add player header include argument to game

--- DeadMansDraw/Game.h
+++ DeadMansDraw/Game.h
@@ -6,6 +6,7 @@
 
 #include <string>
 #include "Card.h"
+#include "Player.h"
 
 class Player;
 
--- DeadMansDraw/Main.cpp
+++ DeadMansDraw/Main.cpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 #include "gameTitle.h"
+#include "Game.h"
 
 int main()
 {
     std::cout << GAME_TITLE << std::endl;
+    //Game game;
+    //game.startGame();
     return 0;
 }

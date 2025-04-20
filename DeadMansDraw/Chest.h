2ec7f97baa879592b44f492484f72a0c920b8dc1
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 02:54:27 +0930

    Add initial cannon header file

---
+++ DeadMansDraw/Chest.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/*
+    Chest card type
+*/
+
+#include "Card.h"
+
+class Chest : public Card {
+public:
+    Chest(int value);
+    CardType type() const;
+    std::string str() const;
+    void play(Game& game, Player& player);
+    void willAddToBank(const Player& player);
+    int getValue() const;
+
+private:
+    int value;
+};

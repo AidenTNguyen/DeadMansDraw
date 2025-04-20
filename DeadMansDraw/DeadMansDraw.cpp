214d9a8320b2f599910e196552c7fd1e4f8521c1
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 02:09:01 +0930

    add initial project commit

---
+++ DeadMansDraw/DeadMansDraw.cpp
@@ -0,0 +1,11 @@
+// DeadMansDraw.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "gameTitle.h"
+
+int main()
+{
+    std::cout << GAME_TITLE << std::endl;
+    return 0;
+}

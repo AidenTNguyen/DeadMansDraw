a10262c86e335e07c36937651c27673ceff9c411
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 03:58:57 +0930

    Add initial map functions

---
+++ DeadMansDraw/Map.cpp
@@ -0,0 +1,28 @@
+#include "Map.h"
+
+using namespace std;
+
+Map::Map(int _value)
+    : Card(CardType::Map), value(_value)
+{
+}
+
+int Map::getValue() const {
+    return value;
+}
+
+Card::CardType Map::type() const {
+    return cardType;
+}
+
+std::string Map::str() const {
+    return "";
+}
+
+void Map::play(Game& game, Player& player) {
+
+}
+
+void Map::willAddToBank(Game& game, const Player& player) {
+
+}

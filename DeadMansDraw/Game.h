1de9ea0960d88f059e10b9c7ea7de2d15bc49d0c
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 04:41:22 +0930

    Add initial game header file

---
+++ DeadMansDraw/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+#pragma once
+
+/*
+    Mainly responsible for player's own play area and bank
+    will hold both name and score
+*/
+
+#include <string>
+#include "Card.h"
+
+class Player;
+
+using CardCollection = std::vector<std::unique_ptr<Card>>;
+
+class Game {
+
+};

f228bfb76e9951bf365c7d323cde472ba8f7c13d
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 03:49:00 +0930

    Add initial chest.cpp functions

---
+++ DeadMansDraw/Chest.cpp
@@ -0,0 +1,28 @@
+#include "Chest.h"
+
+using namespace std;
+
+Chest::Chest(int _value)
+    : Card(CardType::Chest), value(_value)
+{
+}
+
+int Chest::getValue() const {
+    return value;
+}
+
+Card::CardType Chest::type() const {
+    return cardType;
+}
+
+std::string Chest::str() const {
+    return "";
+}
+
+void Chest::play(Game& game, Player& player) {
+
+}
+
+void Chest::willAddToBank(Game& game, const Player& player) {
+
+}

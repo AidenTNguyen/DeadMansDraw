ac61b91fa969bebbe3961e7fd99cbc1938a55d81
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 03:53:17 +0930

    Add initial sword.cpp functions

---
+++ DeadMansDraw/Sword.cpp
@@ -0,0 +1,28 @@
+#include "Sword.h"
+
+using namespace std;
+
+Sword::Sword(int _value)
+    : Card(CardType::Sword), value(_value)
+{
+}
+
+int Sword::getValue() const {
+    return value;
+}
+
+Card::CardType Sword::type() const {
+    return cardType;
+}
+
+std::string Sword::str() const {
+    return "";
+}
+
+void Sword::play(Game& game, Player& player) {
+
+}
+
+void Sword::willAddToBank(Game& game, const Player& player) {
+
+}

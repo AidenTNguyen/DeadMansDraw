1427c718c826ddbbcb3ed5fea0430d7089fcaf06
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 04:03:24 +0930

    Add initial mermaid functions

---
+++ DeadMansDraw/Mermaid.cpp
@@ -0,0 +1,28 @@
+#include "Mermaid.h"
+
+using namespace std;
+
+Mermaid::Mermaid(int _value)
+    : Card(CardType::Mermaid), value(_value)
+{
+}
+
+int Mermaid::getValue() const {
+    return value;
+}
+
+Card::CardType Mermaid::type() const {
+    return cardType;
+}
+
+std::string Mermaid::str() const {
+    return "";
+}
+
+void Mermaid::play(Game& game, Player& player) {
+
+}
+
+void Mermaid::willAddToBank(Game& game, const Player& player) {
+
+}

710fe9a8c0fc2d06ab19aa2fdd74585c5b5f9215
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 03:54:10 +0930

    Add initial hook.cpp functions

---
+++ DeadMansDraw/Hook.cpp
@@ -0,0 +1,28 @@
+#include "Hook.h"
+
+using namespace std;
+
+Hook::Hook(int _value)
+    : Card(CardType::Hook), value(_value)
+{
+}
+
+int Hook::getValue() const {
+    return value;
+}
+
+Card::CardType Hook::type() const {
+    return cardType;
+}
+
+std::string Hook::str() const {
+    return "";
+}
+
+void Hook::play(Game& game, Player& player) {
+
+}
+
+void Hook::willAddToBank(Game& game, const Player& player) {
+
+}

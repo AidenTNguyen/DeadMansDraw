46e2db14f943bda0b88f4c8da1a1ca869c8b39cc
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 16:00:36 +0930

    WIP cannon functionality

--- DeadMansDraw/Cannon.cpp
+++ DeadMansDraw/Cannon.cpp
@@ -1,6 +1,6 @@
 #include "Cannon.h"
 #include "Player.h"
-
+#include "Game.h"
 using namespace std;
 
 Cannon::Cannon(int _value)
@@ -21,6 +21,16 @@ std::string Cannon::str() const {
 
 void Cannon::play(Game& game, Player& player) {
     std::cout << player.getName() << " draws a " << str() << std::endl;
+
+    // Find opponent
+    Player* opponent = nullptr;
+    auto& playerList = game.getPlayerList();
+    for (auto& currentPlayer : playerList) {
+        if (currentPlayer && currentPlayer->getName() != player.getName()) {
+            opponent = currentPlayer.get();
+            break;
+        }
+    }
 }
 
 void Cannon::willAddToBank(Game& game, const Player& player) {

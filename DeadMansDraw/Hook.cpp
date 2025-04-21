8d1bd149ed6820c361fff56ad7fed6176aefe564
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 13:42:33 +0930

    WIP Hook functionality
    i wont lie i be looking at some stack overflow for this lambda function

--- DeadMansDraw/Hook.cpp
+++ DeadMansDraw/Hook.cpp
@@ -24,15 +24,23 @@ std::string Hook::str() const {
 void Hook::play(Game& game, Player& player) {
     std::cout << "  Select a highest-value card from any of the suits in your Bank:" << std::endl;
 
-    std::vector<std::unique_ptr<Card>> highestCards;
+    std::map<Card::CardType, std::vector<std::unique_ptr<Card>>> suitGroups;
 
     for (auto& card : player.getBank()) {
         Card::CardType suit = card->type();
 
-        // new group for each suit
-        if 
+        // Create a group for the suit if it doesn't exist
+        suitGroups[suit].push_back(std::move(card));
+    }
 
-        }
+    std::vector<std::unique_ptr<Card>> highestCards;
+
+    for (auto& suitGroup : suitGroups) {
+        auto& group = suitGroup.second;
+        auto maxCardIterator = std::max_element(group.begin(), group.end(), [](const std::unique_ptr<Card>& cardA, const std::unique_ptr<Card>& cardB) {
+            return cardA->getValue() < cardB->getValue();
+            });
+    }
 
 }
 

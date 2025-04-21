982c6fc7f633e29c5d5bc8180d0607fa732c878b
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 16:42:29 +0930

    WIP Sword

--- DeadMansDraw/Sword.cpp
+++ DeadMansDraw/Sword.cpp
@@ -21,7 +21,14 @@ std::string Sword::str() const {
 }
 
 void Sword::play(Game& game, Player& player) {
-    std::cout << player.getName() << " draws a " << str() << std::endl;
+    std::cout << "  Steal the top card of any suit from the other player's Bank into your play area" << std::endl;
+
+    // Find the opponent
+
+    //if (opponentBank.empty()) {
+    //    std::cout << "  No cards in other player's Bank. Play continues." << std::endl;
+    //    return;
+    //}
 }
 
 void Sword::willAddToBank(Game& game, const Player& player) {

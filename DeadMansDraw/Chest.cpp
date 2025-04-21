531de7de87d2ed47e4418a59ded2baf99b169a38
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 16:13:33 +0930

    WIP chest

--- DeadMansDraw/Chest.cpp
+++ DeadMansDraw/Chest.cpp
@@ -24,6 +24,16 @@ void Chest::play(Game& game, Player& player) {
     std::cout << "  No immediate effect. If banked with a key, draw as many bonus cards from the discard pile as you moved into your Bank." << std::endl;
 }
 
-void Chest::willAddToBank(Game& game, const Player& player) {
+void Chest::willAddToBank(Game& game, Player& player) {
+    
+    bool hasKey = false;
+    CardCollection& bank = player.getBank();
+
+    for (const auto& card : bank) {
+        if (card && card->type() == Card::CardType::Key) {
+            hasKey = true;
+            break;
+        }
+    }
 
 }

3a22143fd03cadee3c3f751b230c3488ace086e3
AidenTNguyen <lamaiden124@gmail.com>
2025-04-21 13:24:54 +0930

    WIP Hook functionality
    Scrap the previous idea. maps all the way

--- DeadMansDraw/Hook.cpp
+++ DeadMansDraw/Hook.cpp
@@ -1,5 +1,6 @@
 #include "Hook.h"
 #include "Player.h"
+#include <map>
 
 using namespace std;
 
@@ -23,20 +24,13 @@ std::string Hook::str() const {
 void Hook::play(Game& game, Player& player) {
     std::cout << "  Select a highest-value card from any of the suits in your Bank:" << std::endl;
 
-    int index = 1;
-    for (const auto& card : player.getBank()) {
-        std::cout << "(" << index++ << ") " << card->str() << std::endl; // Print it out
+    std::map<std::string, std::vector<std::unique_ptr<Card>>> suitGroups;
+
+    for (auto& card : player.getBank()) {
+        Card::CardType suit = card->type();
+        suitGroups[static_cast<int>(suit)].push_back(std::move(card));
     }
 
-    int highestValue = -1;
-    int highestIndex = -1;
-
-    index = 0; // dont think it matters if we  reuse this
-
-    for (const auto& card : player.getBank()) {
-        int cardValue = card->getValue();
-        if (cardValue > highestValue)
-    }
 }
 
 void Hook::willAddToBank(Game& game, const Player& player) {
